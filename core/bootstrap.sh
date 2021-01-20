29c40faa06e9dca8f0b46be08fd1cddae0490d6e
Nate Sales <nate@natesales.net>
2021-01-20 13:36:53 -0800

    enable FRR bgpd

--- core/bootstrap.sh
+++ core/bootstrap.sh
@@ -7,3 +7,7 @@ curl -s https://deb.frrouting.org/frr/keys.asc | sudo apt-key add -
 echo deb https://deb.frrouting.org/frr $(lsb_release -s -c) frr-stable | sudo tee /etc/apt/sources.list.d/frr.list
 sudo apt update
 sudo apt install -y frr frr-pythontools
+
+# Enable FRR bgpd
+sudo sed -i "s/^bgpd=no/bgpd=yes/" /etc/frr/daemons
+sudo systemctl restart frr
