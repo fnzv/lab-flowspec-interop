c5c1fee118636255ee3d8da2ca98835d5f436946
Nate Sales <nate@natesales.net>
2021-01-20 13:46:28 -0800

    set static frr release

--- core/bootstrap.sh
+++ core/bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt install -y gnupg2
 
 # Install FRR
 curl -s https://deb.frrouting.org/frr/keys.asc | sudo apt-key add -
-echo deb https://deb.frrouting.org/frr $(lsb_release -s -c) frr-stable | sudo tee /etc/apt/sources.list.d/frr.list
+echo deb https://deb.frrouting.org/frr buster frr-stable | sudo tee /etc/apt/sources.list.d/frr.list
 sudo apt update
 sudo apt install -y frr frr-pythontools
 
