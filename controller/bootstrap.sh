50a746675238674bbe39908970c135d1be6d64ca
Nate Sales <nate@natesales.net>
2021-01-20 16:07:33 -0800

    fix quotes

--- controller/bootstrap.sh
+++ controller/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deb http://deb.debian.org/debian/ sid main" | sudo tee /etc/apt/sources.list.d/bird2.list
-echo "APT::Default-Release "stable";" | sudo tee /etc/apt/apt.conf.d/default-release
+echo 'APT::Default-Release "stable";' | sudo tee /etc/apt/apt.conf.d/default-release
 
 sudo apt update
 sudo apt install -y -t sid bird2
